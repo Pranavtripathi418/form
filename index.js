e91459d9a7c46fda345667bf5963ae7035d2b16c
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-05-29 01:20:44 +0530

    edit functionality added

--- index.js
+++ index.js
@@ -28,12 +28,30 @@ Delete.onclick = function(){
     deleteUser(userDetails);
     parent.removeChild(child);
 }
+const editButton = document.createElement('button');
+editButton.textContent = "Edit";
+editButton.onclick = function() {
+    editUser(userDetails);
+    parent.removeChild(child);
+};
 child.textContent= userDetails.username+ '-' +userDetails.email+ '-' + userDetails.phone;
 child.appendChild(Delete)
+child.appendChild(editButton)
 parent.appendChild(child);
+
 }
 
 function deleteUser(userDetails) {
     let users = JSON.parse(localStorage.getItem('User Details')) 
  
+}
+
+function editUser(userDetails) {
+    
+    document.getElementById('username').value = userDetails.username;
+    document.getElementById('email').value = userDetails.email;
+    document.getElementById('phone').value = userDetails.phone;
+
+   
+    deleteUser(userDetails);
 }
